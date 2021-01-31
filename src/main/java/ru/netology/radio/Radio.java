d27145148be4a644d242c9d95db91fa4fb7dde70
Maxim Kazantsev <aeontal@gmail.com>
2021-01-31 15:27:27 +0500

    Выгрузка проекта

---
+++ src/main/java/ru/netology/radio/Radio.java
@@ -0,0 +1,75 @@
+package ru.netology.radio;
+
+import javax.swing.*;
+
+public class Radio {
+    private int channelNum;
+    private int channelMax = 9;
+    private int channelMin = 1;
+    private int volumeLevel;
+    private int volumeMax = 10;
+    private int volumeMin = 0;
+
+    public void setChannelNum(int channelNum) {
+        if (channelNum > channelMax) {
+            this.channelNum = channelMax;
+        } else if (channelNum < channelMin) {
+            this.channelNum = channelMin;
+        } else {
+            this.channelNum = channelNum;
+        }
+    }
+
+    public int getChannelNum() {
+        return channelNum;
+    }
+
+    public void setVolumeLevel(int volumeLevel) {
+        if (volumeLevel > volumeMax) {
+            this.volumeLevel = volumeMax;
+        } else {
+            this.volumeLevel = volumeLevel;
+        }
+    }
+
+    public int getVolumeLevel() {
+        return volumeLevel;
+    }
+
+    public int increaseVolume() {
+        if (volumeLevel < volumeMax) {
+            this.volumeLevel++;
+        } else {
+            this.volumeLevel = volumeMax;
+        }
+        return volumeLevel;
+    }
+
+    public int decreaseVolume() {
+        if (volumeLevel > volumeMin) {
+            this.volumeLevel--;
+        } else {
+            this.volumeLevel = volumeMin;
+        }
+        return volumeLevel;
+    }
+
+    public int nextChannel() {
+        if (channelNum < channelMax) {
+            this.channelNum++;
+        } else {
+            this.channelNum = channelMin;
+        }
+        return channelNum;
+    }
+
+    public int previousChannel() {
+        if (channelNum > channelMin) {
+            this.channelNum--;
+        } else {
+            this.channelNum = channelMax;
+        }
+        return channelNum;
+    }
+
+}
